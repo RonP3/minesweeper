b7a0c6bdf25d697412ba38d7eab07c2b7bbddd88
RonP3 <ron.paz@gmail.com>
2019-12-16 16:06:00 +0200

    Updates

---
+++ precache-manifest.40b62b400c0973fb49076e69234ff4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81ec15cc7b23aba8033961f3a663d7b",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "b708421a32bc3f556958",
+    "url": "/minesweeper/static/css/main.086c016d.chunk.css"
+  },
+  {
+    "revision": "f9c471fe58af964bdd0d",
+    "url": "/minesweeper/static/js/2.1b65ab62.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/minesweeper/static/js/2.1b65ab62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b708421a32bc3f556958",
+    "url": "/minesweeper/static/js/main.22f24b7e.chunk.js"
+  },
+  {
+    "revision": "81fac3eb777183ec555d",
+    "url": "/minesweeper/static/js/runtime-main.70dc93c3.js"
+  }
+]);
